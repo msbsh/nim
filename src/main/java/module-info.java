6802331853996b241544197dd5d6cb2a53bd4968
Martin James <martin.james@iteratec.de>
2018-01-11 13:53:44 +0100

    feat - java 9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+@SuppressWarnings({"removal"})
+module de.coding.kata.nim {
+
+    requires spring.web;
+    requires spring.data.jpa;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires java.xml.bind;
+    requires java.xml.ws.annotation;
+    requires hibernate.jpa;
+    requires lombok;
+
+}
